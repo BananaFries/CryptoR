01fd22ad8694a4ae5925318e40c9d7f20430a185
Skad0sh <57108056+Skad0sh@users.noreply.github.com>
2019-11-17 22:40:56 +0530

    Updated ceaserD function
    Updated ceaserD funtion . Added a loop to it just like in ceaserE function so that it will not end the program after a wrong input.

--- moduleTest.py
+++ moduleTest.py
@@ -22,18 +22,29 @@ def ceaserE(message, key):
 
 
 
-def ceaserD(message, key):
-    cipher = ""
-    for letter in message:
-        if letter == " ":
-            cipher = cipher + " "
-        elif letter in "0123456789":
-            print("Numbers are not allowed in message")
+def ceaserD(cipher, key):
+     message = ""
+    for letter in cipher:
+        a = 1
+        while(a == 1):
+            if letter == " ":
+                message = message + " "
+                b = 1
+                a = 0
+            elif letter in "0123456789":
+                print("Numbers are not allowed in message")
+                a = 0
+                b = 0
+                break
+            else:
+                message = message + chr(ord(letter)+key)
+                a = 0
+                b = 1
+        if b == 0:
             break
-        else:
-            cipher = cipher + chr(ord(letter) - key)
-    print(cipher)
+    print("Here is your encrypted message: "+message)
 
+   
 
 
 
