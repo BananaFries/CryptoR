594bd40814404df1de668ae77d19ba0430c0575e
BananaFries <30596952+BananaFries@users.noreply.github.com>
2019-12-02 17:29:06 +0530

    indents

--- moduleTest.py
+++ moduleTest.py
@@ -21,7 +21,8 @@ def ceaserE(message, key):
     print("Here is your encrypted message: "+cipher)
 
 
-
+#there is a indent error in the for part so just make the indents proper . use an ide or an editor dont edit here in github
+# if u use an editor u will be able to see the indent lines to follow
 def ceaserD(cipher, key):
      message = ""
     for letter in cipher:
